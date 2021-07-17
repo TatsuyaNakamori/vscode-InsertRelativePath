60f6b7b2aefe23408cd8e8207f3179fd1d859d80
TatsuyaNakamori <tnakamori.job.734@gmail.com>
2021-07-17 22:46:59 +0900

    In progress

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,89 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// ============================================================
+// Copyright (c) 2021 Tatsuya Nakamori. All rights reserved.
+// See LICENSE in the project root for license information.
+// ============================================================
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "insertrelpath" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "insertrelpath" is now active!');
+    setContext();
+    vscode.workspace.onDidCloseTextDocument((event) => {setContext()})
+    vscode.window.onDidChangeActiveTextEditor((event) => {setContext()})
+    vscode.window.onDidChangeTextEditorSelection((event) => {setContext()})
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('insertrelpath.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from insertrelpath!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.commands.registerCommand('insertrelpath.insertRelPath', (pathInfo) => {
+            insertRelPath(pathInfo.fsPath);
+    }));
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+
+async function setContext() {
+    const editor = vscode.window.activeTextEditor;
+
+    if (editor) {
+        vscode.commands.executeCommand('setContext', 'insertrelpath.editor.opening', true);
+    } else {
+        vscode.commands.executeCommand('setContext', 'insertrelpath.editor.opening', false);
+    }
+}
+
+interface RelPathOptions {
+    "format"?: string,
+    "separator"?: ("/"|"\\"|"Adapt to OS"),
+    "withExtension"?: boolean,
+}
+
+function getParsedConfig(): RelPathOptions {
+    const config = vscode.workspace.getConfiguration("InsertRelPath");
+
+    const relPathOptions:RelPathOptions = {
+        "format": config.get("format"),
+        "separator": config.get("separator"),
+        "withExtension": config.get("withExtension")
+    }
+    return relPathOptions
+}
+
+async function insertRelPath(pathTo:string) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) { return }
+
+    const relPathOptions: RelPathOptions = getParsedConfig();
+    console.log(relPathOptions);
+
+    // Path From
+    let pathFrom = editor.document.uri.fsPath;
+    pathFrom = path.dirname(pathFrom);
+
+    // Generate and format relative paths
+    let relPath = path.relative(pathFrom, pathTo);
+
+    const osSeparator = path.sep;
+    const separator = relPathOptions["separator"];
+    relPath = relPath.replace(/\\/g, '/');
+
+    if (!relPathOptions["withExtension"]) {
+        const parse = path.parse(relPath);
+        if (parse.dir) {
+            relPath = `${parse.dir}/${parse.name}`;
+        } else {
+            relPath = parse.name;
+        }
+    }
+
+    // Insert text and adjust the selection.
+    await editor.edit(editBuilder => {
+        editBuilder.replace(editor.selection, relPath);
+    });
+    const selectionEnd = editor.selection.end;
+    editor.selection = new vscode.Selection(selectionEnd, selectionEnd);
+
+    // Focus on the editor.
+    vscode.window.showTextDocument(editor.document);
+}
+
