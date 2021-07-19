2e1c0836c094216874161a4da1919397728a33d7
TatsuyaNakamori <tnakamori.job.734@gmail.com>
2021-07-20 01:58:47 +0900

    Added the ability to insert relative paths. [Almost done. Minor bug fixes, etc. to come].

--- src/extension.ts
+++ src/extension.ts
@@ -35,6 +35,10 @@ interface StringKeyObject {
     [key: string]: string;
 }
 
+interface RelPathTable {
+    [key: string]: string|undefined;
+}
+
 function getParsedConfig(): string[]|undefined {
     let formatList:string[] = [];
 
@@ -50,93 +54,150 @@ function getParsedConfig(): string[]|undefined {
         }
     }
 
-    console.log(formatList);
     if (!formatList.length) {
         return
     }
     return formatList
 }
 
-function generateStringTtoInsert(editor:vscode.TextEditor, pathTo:string, format:string): string|undefined {
-    console.log(format);
+function removeExtension(relPath:string|undefined):string|undefined {
+    if (!relPath) { return }
 
-
-    return format
-
-    // // Path From
-    // let pathFrom = editor.document.uri.fsPath;
-    // pathFrom = path.dirname(pathFrom);
-
-    // // Generate and format relative paths
-    // let relPath = path.relative(pathFrom, pathTo);
-    // console.log(relPath);
-
-    // // Perform the process of removing file extensions.
-    // if (formatList["withoutExtension"]) {
-    //     const parse = path.parse(relPath);
-    //     if (parse.dir) {
-    //         relPath = `${parse.dir}${path.sep}${parse.name}`;
-    //     } else {
-    //         relPath = parse.name;
-    //     }
-    // }
-
-    // // Convert separator
-    // const separatorToUse = formatList["separator"];
-    // if (separatorToUse == "Adapt to OS") {
-    //     // Since relPath is already the os separator, there is no need to replace it.
-    // } else if (path.sep == "\\" && separatorToUse == "/") {
-    //     relPath = relPath.replace(/\\/g, "/");
-    // } else if (path.sep == "/" && separatorToUse == "\\") {
-    //     relPath = relPath.replace(/\//g, "\\");
-    // }
-
-    // // Incorporate relative paths into the format string.
-    // let format = formatList["format"];
-    // format = format.replace(/\\n/g, "\n");
-    // format = format.replace(/\\r/g, "\r");
-    // format = format.replace(/\\b/g, "\b");
-    // format = format.replace(/\\f/g, "\f");
-    // format = format.replace(/\\t/g, "\t");
-    // format = format.replace(/\\v/g, "\v");
-    // const replaceStr = format.replace(/\${RELPATH}/g, relPath);
-
-    // return
+    const parse = path.parse(relPath);
+    if (parse.dir.endsWith("/") || parse.dir.endsWith("\\")) {
+        relPath = `${parse.dir}${parse.name}`;
+    } else if (parse.dir) {
+        relPath = `${parse.dir}${path.sep}${parse.name}`;
+    } else {
+        relPath = parse.name;
+    }
+    return relPath
 }
 
-async function editBuilderReplace(editor:vscode.TextEditor, replaceStr:string) {
-    // Insert text and adjust the selection.
-    await editor.edit(editBuilder => {
-        editBuilder.replace(editor.selection, replaceStr);
-    });
+function convertSeparator(relPath:string|undefined, sep:string|undefined):string|undefined {
+    if (!relPath) { return }
+    if (sep === undefined) {
+        sep = "/";
+    }
 
-    const selectionEnd = editor.selection.end;
-    editor.selection = new vscode.Selection(selectionEnd, selectionEnd);
+    if (sep == "OS") {
+        // Since relPath is already the os separator, there is no need to replace it.
+    } else if (path.sep == "\\" && sep == "/") {
+        relPath = relPath.replace(/\\/g, "/");
+    } else if (path.sep == "/" && sep == "\\") {
+        relPath = relPath.replace(/\//g, "\\");
+    }
+
+    return relPath
+}
+
+function generateStringToInsert(editor:vscode.TextEditor, pathTo:string, format:string): string|undefined {
+    const regFormat = /^(\[(?<separator>\/|\\|OS)\])?(?<format>.*)/
+
+    const match = regFormat.exec(format);
+    if (!match?.groups) { return }
+
+    const sepText = match.groups["separator"]
+    let formatText = match.groups["format"]
+    if (!formatText) { return }
+
+    // Write the relative path information into the table.
+    const docFsPath = editor.document.uri.fsPath;
+    const curFolder = path.dirname(docFsPath);
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    const workspaceFolder = workspaceFolders? workspaceFolders[0]: undefined;
+
+    let relPathTable:RelPathTable = {};
+    // Generating relative paths
+    // ==================================================
+    // "{RELPATH}":                // img/pic.png
+    // "{RELPATH_ROOT}":           // /img/pic.png
+    // "{RELPATH_CURDIR}":         // ./img/pic.png
+    // "{RELPATH_FROMFILE}":       // ../img/pic.png
+    // "{RELPATH_NOEXT}":          // img/pic
+    // "{RELPATH_ROOT_NOEXT}":     // /img/pic
+    // "{RELPATH_CURDIR_NOEXT}":   // ./img/pic
+    // "{RELPATH_FROMFILE_NOEXT}": // ../img/pic
+    // "{FILENAME}":               // pic.png
+    // "{FILENAME_SEP}":           // /pic.png
+    // "{FILENAME_NOEXT}":         // pic
+    // "{FILENAME_SEP_NOEXT}":     // /pic
+    // ==================================================
+    relPathTable["{RELPATH}"] = path.relative(curFolder, pathTo);
+    if (workspaceFolder) {
+        const rootFolder = workspaceFolder.uri.fsPath;
+        const relpath = path.relative(rootFolder, pathTo);
+        relPathTable["{RELPATH_ROOT}"] = `${path.sep}${relpath}`;
+    }
+    relPathTable["{RELPATH_CURDIR}"] = `.${path.sep}${relPathTable["{RELPATH}"]}`;
+    relPathTable["{RELPATH_FROMFILE}"] = path.relative(docFsPath, pathTo);
+    // File name only and file name without extension
+    relPathTable["{FILENAME}"] = path.basename(pathTo);
+    relPathTable["{FILENAME_SEP}"] = `${path.sep}${path.basename(pathTo)}`;
+    // Versions that exclude the extension
+    relPathTable["{RELPATH_NOEXT}"]          = removeExtension(relPathTable["{RELPATH}"]);
+    relPathTable["{RELPATH_ROOT_NOEXT}"]     = removeExtension(relPathTable["{RELPATH_ROOT}"]);
+    relPathTable["{RELPATH_CURDIR_NOEXT}"]   = removeExtension(relPathTable["{RELPATH_CURDIR}"]);
+    relPathTable["{RELPATH_FROMFILE_NOEXT}"] = removeExtension(relPathTable["{RELPATH_FROMFILE}"]);
+    relPathTable["{FILENAME_NOEXT}"]         = removeExtension(relPathTable["{FILENAME}"]);
+    relPathTable["{FILENAME_SEP_NOEXT}"]     = removeExtension(relPathTable["{FILENAME_SEP}"]);
+
+    // Convert Separator
+    for (const key in relPathTable) {
+        if (Object.prototype.hasOwnProperty.call(relPathTable, key)) {
+            relPathTable[key] = convertSeparator(relPathTable[key], sepText);
+        }
+    }
+
+    // Incorporate relative paths into the format string.
+    formatText = formatText.replace(/\\n/g, "\n");
+    formatText = formatText.replace(/\\r/g, "\r");
+    formatText = formatText.replace(/\\b/g, "\b");
+    formatText = formatText.replace(/\\f/g, "\f");
+    formatText = formatText.replace(/\\t/g, "\t");
+    formatText = formatText.replace(/\\v/g, "\v");
+
+    for (const key in relPathTable) {
+        if (Object.prototype.hasOwnProperty.call(relPathTable, key)) {
+
+            const relpath = relPathTable[key];
+            if (relpath) {
+                const regPlaceholder = new RegExp(key, "g");
+                formatText = formatText.replace(regPlaceholder, relpath);
+            }
+        }
+    }
+
+    return formatText
 }
 
 async function insertRelPath(pathTo:string) {
     const editor = vscode.window.activeTextEditor;
     if (!editor) { return }
 
+    // Get Config
     const formatList: string[]|undefined = getParsedConfig();
     if (!formatList) { return }
 
+    // Generate String To Insert
     let replaceStrList:string[] = [];
     for (let i = 0; i < formatList.length; i++) {
-        const insertStr = generateStringTtoInsert(editor, pathTo, formatList[i]);
+        const insertStr = generateStringToInsert(editor, pathTo, formatList[i]);
         if (insertStr) {
             replaceStrList.push(insertStr);
         }
     }
 
     if (replaceStrList.length == 1) {
-        editBuilderReplace(editor, replaceStrList[0]);
+        const snippet = new vscode.SnippetString(replaceStrList[0]);
+        editor.insertSnippet(snippet);
 
     } else if (replaceStrList.length > 1) {
         const selectedItem:string|undefined = await vscode.window.showQuickPick(replaceStrList);
 
         if (selectedItem) {
-            editBuilderReplace(editor, selectedItem);
+            const snippet = new vscode.SnippetString(selectedItem);
+            editor.insertSnippet(snippet);
         }
     }
 
